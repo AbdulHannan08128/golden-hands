51b37c31d2b5d115c5e1b5c94ad68766ef2b681b
AbdulHannan08128 <154576645+AbdulHannan08128@users.noreply.github.com>
2024-05-24 17:42:45 +0530

    V1

---
+++ src/app/admin/Dashboard.js
@@ -0,0 +1,100 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import Cards from "./Cards";
+import CircularProgress from "@mui/material/CircularProgress";
+import { PieChart } from "@mui/x-charts/PieChart";
+import { ADMIN } from "../../../utils/API";
+export const dynamic = "force-dynamic";
+
+const Dashboard = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    async function getData() {
+      const response = await ADMIN();
+      setData(response?.data);
+    }
+    getData();
+  }, []);
+
+  const appointments = data?.APPOINTMENTS;
+
+  const unRespondedCount = appointments?.filter((appointment) => {
+    return appointment?.STATUS === "PENDING";
+  })?.length;
+
+  const respondedCount = appointments?.filter((appointment) => {
+    return appointment?.STATUS !== "PENDING";
+  })?.length;
+
+  async function revalidateData() {
+    setData(undefined);
+    const response = await ADMIN();
+    setData(response?.data);
+  }
+
+  return (
+    <>
+      <div className="">
+        {data ? (
+          <>
+            <button
+              className="rounded-3xl border-black bg-green-500 px-5 py-2 text-white active:bg-black active:dark:bg-white active:dark:text-black"
+              onClick={revalidateData}
+            >
+              Revalidate
+            </button>
+            <Cards data={data} />
+            <div className="">
+              <div className="mt-20 h-[300px] w-[500px] max-w-[90vw] overflow-y-hidden overflow-x-scroll rounded-3xl shadow-xl dark:bg-slate-400">
+                <PieChart
+                  series={[
+                    {
+                      data: [
+                        {
+                          id: "Unresponded",
+                          value: unRespondedCount,
+                          label: "Unresponded",
+                          color: "red",
+                        },
+                        {
+                          id: "Responded",
+                          value: respondedCount,
+                          label: "Responded",
+                          color: "green",
+                        },
+                      ],
+                      innerRadius: 30,
+                      outerRadius: 100,
+                      paddingAngle: 5,
+                      cornerRadius: 5,
+                      startAngle: 0,
+                      endAngle: 360,
+                      cx: 150,
+                      cy: 150,
+                      highlightScope: { faded: "global", highlighted: "item" },
+                      faded: {
+                        innerRadius: 30,
+                        additionalRadius: -30,
+                        color: "gray",
+                      },
+                    },
+                  ]}
+                  width={400}
+                  height={400}
+                  className="overflow-scroll"
+                />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="flex h-[100%] w-[100%] items-center justify-center align-middle backdrop-blur-sm">
+            <CircularProgress color="inherit" />
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
